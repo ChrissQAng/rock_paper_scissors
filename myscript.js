4e5e50808e17a8418055917c096438cf8711f766
ChrissQAng <132443487+ChrissQAng@users.noreply.github.com>
2023-07-12 09:29:01 +0200

    function not working maybe global variable issue branching and asking somebody

--- myscript.js
+++ myscript.js
@@ -1,1 +1,13 @@
-console.log("Hello Hell!")
+const computerSelection
+
+
+function getRandomInteger() {
+    return Math.floor(Math.random() * 3) +1;
+}
+
+function getComputerChoice() {
+    if (getRandomInteger() == 1) {computerSelection = "rock"};
+    else if (getRandomInteger() == 2) {computerSelection = "paper"};
+    else {computerSelection = "scissors"};
+}
+console.log(computerSelection)
