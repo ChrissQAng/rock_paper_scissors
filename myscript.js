d666fff945401540329c7a7ab8bbb3576557b2a8
ChrissQAng <132443487+ChrissQAng@users.noreply.github.com>
2023-07-13 13:48:08 +0200

    game ready exept declaration of winner missing

--- myscript.js
+++ myscript.js
@@ -1,3 +1,5 @@
+let playerPoints = 0
+let computerPoints = 0
 
 function getRandomInteger() {
     return Math.floor(Math.random() * 3) +1;
@@ -19,12 +21,16 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection ===  computerSelection) {
       return 'Draw!';
     } else if (playerSelection === "rock" && computerSelection == "scissors") {
+        playerPoints++;
         return `You win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection === "paper" && computerSelection == "rock") {
+        playerPoints++;
         return `You win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection === "scissors" && computerSelection == "paper") {
+        playerPoints++;
         return `You win! ${playerSelection} beats ${computerSelection}`;
     } else {
+        computerPoints++;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 };
@@ -37,8 +43,9 @@ console.log(playRound(playerSelection, computerSelection));*/
 for (let i=1; i <= 5; i++) { 
 let computerSelection = getComputerSelection()
 let playerSelection = prompt('Enter either rock, paper or scissors').toLowerCase(); 
+console.log(`round: ` + i);
 console.log('player: ' + playerSelection);
 console.log('computer: ' + computerSelection);
-console.log(i);
 console.log(playRound(playerSelection, computerSelection));
+console.log('playerpoints :' + playerPoints + '  computerpoints :' + computerPoints)
 }
